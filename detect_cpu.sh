922908f07d4503d5cd80bf2a6a64a83c0f224acb
prammmoe <ikhwanpramuditha05@gmail.com>
2024-04-01 12:52:39 +0700

    add image

--- detect_cpu.sh
+++ detect_cpu.sh
@@ -1,1 +1,1 @@
-python detect_dual.py --source './data/images/balkotblur.jpg' --img 640 --device cpu --weights './yolov9-e.pt' --name yolov9_c_640_detect
+python detect_dual.py --source './data/images/testini.jpg' --img 640 --device cpu --weights './yolov9-e.pt' --name yolov9_c_640_detect
